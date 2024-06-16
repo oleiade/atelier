5c1409ae8a783ba333ce4b0a151f68cf986d2b66
oleiade <theo@crevon.me>
2024-06-16 11:10:33 +0200

    Document package

--- atelier.go
+++ atelier.go
@@ -1,1 +1,6 @@
+// Package atelier is a Go library providing a set of algorithms, data structures, APIs and helpers to resort to
+// when in need.
+//
+// It is essentially my personal library of Go code, in a single place, easily accessible. Types, tools, helpers, that
+// I use on a recurrent basis, and that I don't want to rewrite every time I need them.
 package atelier
